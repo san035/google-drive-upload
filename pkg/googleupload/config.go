a72c670daf1626f03dc03b6ee377d304f620596e
ASkorohodov <ASkorohodov@mtt.ru>
2026-01-18 19:44:36 +0300

    update load config

--- pkg/googleupload/config.go
+++ pkg/googleupload/config.go
@@ -3,42 +3,47 @@ package googleupload
 import (
 	"fmt"
 
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/chinayin/gox/config"
 )
 
 const ConfigFilyDefault = "config.yaml"
 
 type Config struct {
-	OAuthCallbackHostPort string             `yaml:"oauth_callback_host_port" default:"localhost:8080"` // Хост и порт для OAuth callback (по умолчанию "localhost:80909")
-	ConfigGoogleDrives    ConfigGoogleDrives `yaml:"config_google_drives"`
+	OAuthCallbackHostPort string             `yaml:"oauth_callback_host_port" mapstructure:"oauth_callback_host_port" default:"localhost:8080"` // Хост и порт для OAuth callback (по умолчанию "localhost:8080")
+	ConfigGoogleDrives    ConfigGoogleDrives `yaml:"config_google_drives" mapstructure:"config_google_drives"`
 }
 
 type ConfigGoogleDrives []*ConfigGoogleDrive
 
 type ConfigGoogleDrive struct {
-	Id                    string `yaml:"id" envconfig:"ID"`
-	GoogleCredentialsFile string `yaml:"google_credentials_file" envconfig:"GOOGLE_CREDENTIALS_FILE"`
-	UploadCopiesCount     int    `yaml:"upload_copies_count" envconfig:"UPLOAD_COPIES_COUNT"`
-	FolderID              string `yaml:"folder_id" envconfig:"FOLDER_ID"`
+	Id                    string `yaml:"id" mapstructure:"id" default:"0"`
+	GoogleCredentialsFile string `yaml:"google_credentials_file" mapstructure:"google_credentials_file" default:"google_credentials.json"`
+	UploadCopiesCount     int    `yaml:"upload_copies_count" mapstructure:"upload_copies_count" default:"3"`
+	FolderID              string `yaml:"folder_id" mapstructure:"folder_id"`
 }
 
-// LoadConfig загружает конфигурацию из YAML файлов с помощью cleanenv
+// LoadConfig загружает конфигурацию из YAML файлов с помощью gox/config
 // Если файлы не указаны, использует config.yaml
+// Поддерживает значения по умолчанию из тегов default и переменные окружения
 func LoadConfig(yamlFiles ...string) (*Config, error) {
 	// Если файлы не указаны, используем config.yaml по умолчанию
 	if len(yamlFiles) == 0 {
 		yamlFiles = []string{ConfigFilyDefault}
 	}
 
-	config := &Config{}
+	// Создаём загрузчик конфигурации
+	loader := config.NewLoader()
 
-	// Загружаем данные из указанных YAML файлов с помощью cleanenv
+	cfg := &Config{}
+
+	// Загружаем данные из указанных YAML файлов
+	// gox/config автоматически применяет значения по умолчанию и переменные окружения
 	// Последующие файлы перезаписывают значения из предыдущих
 	for _, file := range yamlFiles {
-		if err := cleanenv.ReadConfig(file, config); err != nil {
-			return nil, fmt.Errorf("ошибка чтения файла конфигурации %s: %v", file, err)
+		if err := loader.Load(file, cfg); err != nil {
+			return nil, fmt.Errorf("ошибка загрузки файла конфигурации %s: %v", file, err)
 		}
 	}
 
-	return config, nil
+	return cfg, nil
 }
